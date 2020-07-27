e9059cc7edd7e1fb8d7c0852066e93039a3784ba
root <root@regminint.suse.site>
2020-07-27 10:55:13 +0000

    keep v4 hierarchy in the registry

--- push-regminint.sh
+++ push-regminint.sh
@@ -3,7 +3,7 @@ echo "$(podman images | grep registry.suse.com)" | while read image; do
   IMG_TAG=$(echo "$image" | awk '{print $2}')
   IMG_NAME=$(echo "${IMG_SRC}" | awk -F"/" '{ print $NF }')
   IMG_ID=$(echo "${image}" | awk '{ print $3 }')
-  podman tag ${IMG_ID} regminint.suse.site:80/caasp4/${IMG_NAME}:${IMG_TAG}
-  podman push regminint.suse.site:80/caasp4/${IMG_NAME}:${IMG_TAG}
+  podman tag ${IMG_ID} regminint.suse.site:80/caasp/v4/${IMG_NAME}:${IMG_TAG}
+  podman push regminint.suse.site:80/caasp/v4/${IMG_NAME}:${IMG_TAG}
 done
 
